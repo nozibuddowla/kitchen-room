266ceeaea1c62d3ee956530b7cc636ace42aef0d
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-10-27 03:22:54 +0600

    add load data functionalities using use()hook and state lifting and add to cooking list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
+import { Suspense } from 'react';
 import './App.css'
 import Heading from './components/Heading'
 import Navbar from './components/Navbar'
+import OrderContainer from './components/OrderContainer';
+import { Hourglass } from 'lucide-react';
+import { ToastContainer } from 'react-toastify';
+
+const loadOrders = fetch("/orders.json").then(res => res.json());
 
 function App() {
-
+  const ordersPromise = loadOrders;
+  
   return (
     <>
       <header className='w-11/12 mx-auto py-4'>
@@ -13,6 +20,12 @@ function App() {
       <section>
         <Heading>Kitchen Room</Heading>
       </section>
+      <section>
+        <Suspense fallback={`Loading. . . . ⏳`}>
+          <OrderContainer promise={ordersPromise} />
+        </Suspense>
+      </section>
+      <ToastContainer />
     </>
   )
 }
---
+++ src/components/States.jsx
@@ -0,0 +1,35 @@
+import { CookingPot, ScrollText, TicketCheck } from 'lucide-react';
+import React from 'react';
+
+const States = ({totalOrder, totalCooking}) => {
+    return (
+        <div className='w-11/12 mx-auto py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            <div className="border-4 border-dotted rounded-2xl border-[#FCB700] p-5">
+                <div className="flex flex-col lg:flex-row items-center justify-between">
+                    <ScrollText className="animate-pulse" color="#FCB700" size={100} />
+                    <div className="text-xl text-center">
+                        Current Orders <h2 className="text-6xl font-bold">{totalOrder}</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="border-4 border-dotted rounded-2xl border-[#FCB700] p-5">
+                <div className="flex flex-col lg:flex-row items-center justify-between">
+                    <CookingPot className="animate-pulse" color="#FCB700" size={100} />
+                    <div className="text-xl text-center">
+                        Currently Cooking <h2 className="text-6xl font-bold">{totalCooking}</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="border-4 border-dotted rounded-2xl border-[#FCB700] p-5">
+                <div className="flex flex-col lg:flex-row items-center justify-between">
+                    <TicketCheck className="animate-pulse" color="#FCB700" size={100} />
+                    <div className="text-xl text-center">
+                        Ready to Serve <h2 className="text-6xl font-bold">{0}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default States;
